ae4efb15fd2ddb09251ef2bc15a2e3abb5967859
irene.benedetto <irene.benedetto@h-farm.com>
2023-03-06 11:25:49 +0100

    added gradio interface

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+# This software may be used and distributed according to the terms of the 
+GNU General Public License version 3.
 
 from typing import Tuple
 import os
@@ -11,7 +12,8 @@ import json
 
 from pathlib import Path
 
-from fairscale.nn.model_parallel.initialize import initialize_model_parallel
+from fairscale.nn.model_parallel.initialize import 
+initialize_model_parallel
 
 from llama import ModelArgs, Transformer, Tokenizer, LLaMA
 
@@ -41,7 +43,8 @@ def load(
     checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
     assert world_size == len(
         checkpoints
-    ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
+    ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is 
+{world_size}"
     ckpt_path = checkpoints[local_rank]
     print("Loading")
     checkpoint = torch.load(ckpt_path, map_location="cpu")
@@ -69,45 +72,25 @@ def main(
     temperature: float = 0.8,
     top_p: float = 0.95,
     max_seq_len: int = 512,
-    max_batch_size: int = 32,
+    max_batch_size: int = 6,
 ):
     local_rank, world_size = setup_model_parallel()
     if local_rank > 0:
         sys.stdout = open(os.devnull, "w")
 
     generator = load(
-        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, 
+max_batch_size
     )
 
     prompts = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        "Building a website can be done in 10 simple steps:\n",
-        # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
-        """Tweet: "I hate it when my phone battery dies."
-Sentiment: Negative
-###
-Tweet: "My day has been 👍"
-Sentiment: Positive
-###
-Tweet: "This is the link to the article"
-Sentiment: Neutral
-###
-Tweet: "This new music video was incredibile"
-Sentiment:""",
-        """Translate English to French:
-
-sea otter => loutre de mer
-
-peppermint => menthe poivrée
-
-plush girafe => girafe peluche
-
-cheese =>""",
+        # For these prompts, the expected answer is the natural 
+continuation of the prompt
+        "Sono un essere umano, quindi un mammifero. Sono un coccodrillo 
+quindi un rettile. Sono un pavone quindi un ",
     ]
     results = generator.generate(
-        prompts, max_gen_len=256, temperature=temperature, top_p=top_p
+        prompts, max_gen_len=10, temperature=temperature, top_p=top_p
     )
 
     for result in results:
@@ -117,3 +100,4 @@ cheese =>""",
 
 if __name__ == "__main__":
     fire.Fire(main)
+
